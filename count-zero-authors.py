380551239f3730e30888eedd83e0bd759ec642f4
emeryberger <emery.berger@gmail.com>
2016-05-25 16:44:46 +0200

    More cleanup.

--- count-zero-authors.py
+++ count-zero-authors.py
@@ -187,8 +187,8 @@ else:
 
 for k in facultydict:
     if (intauthors_gl.has_key(k)):
-        if (intauthors_gl[k] == 4):
-            print str(k).encode('utf8') + " : " + str(intauthors_gl[k]).encode('utf8')
+        if (intauthors_gl[k] == 5):
+            print k.encode('utf8') + " : " + str(intauthors_gl[k]).encode('utf8')
 #    else:
 #            print str(k).encode('utf8') + " : 0"
     
