651250feffb7095f0a60b7442c6ef529282ff195
emeryberger <emery.berger@gmail.com>
2016-07-29 18:41:02 -0400

    Updated HPC conferences.

--- util/csrankings.py
+++ util/csrankings.py
@@ -45,7 +45,7 @@ def pagecount(input):
 areadict = {
     # Max three most selective venues per area for now.
     'proglang' : ['POPL', 'PLDI', 'OOPSLA'],
-    'highperf' : ['SC', 'PPOPP'],
+    'highperf' : ['SC', 'HPDC', 'ICS'],
     'logic' : ['CAV', 'CAV (1)', 'CAV (2)', 'LICS', 'CSL-LICS'],
     'softeng' : ['ICSE', 'ICSE (1)', 'ICSE (2)', 'SIGSOFT FSE', 'ESEC/SIGSOFT FSE', 'ASE'],
     'opsys' : ['SOSP', 'OSDI', 'EuroSys', 'USENIX Annual Technical Conference', 'USENIX Annual Technical Conference, General Track'],
