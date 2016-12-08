57eae0199163911dd10d2327b53688ede49c9f04
emeryberger <emery.berger@gmail.com>
2016-12-08 09:31:00 -0500

    Updated to trim more bogus links.

--- util/make-web-pages.py
+++ util/make-web-pages.py
@@ -21,7 +21,10 @@ import requests
 
 facultydict = csv2dict_str_str('faculty-affiliations.csv')
 homepages = csv2dict_str_str('homepages.csv')
-with open("homepages.csv", mode='a') as outfile:
+# Trim out LinkedIn and RateMyProfessors sites, etc.
+trim = ['ratemyprofessors.com', 'linkedin.com', 'wikipedia.org','2016','2015','.pdf']
+
+with open("homepages.csv", mode="a") as outfile:
     facultydictkeys = list(facultydict.keys())
     random.shuffle(facultydictkeys)
     for name in facultydictkeys:
@@ -31,33 +34,34 @@ with open("homepages.csv", mode='a') as outfile:
             match = re.search('www.google.com', homepages[name])
             if (match == None):
                 continue
-        n1 = name
+        str = name + ' ' + facultydict[name]
         name = name.decode('utf8')
         # Grab first result from Google search.
-        str = n1 + ' ' + facultydict[n1]
         results = google.search(str, stop=1)
         actualURL = "FIXME"
-        # Trim out LinkedIn and RateMyProfessors site....
         for url in results:
             actualURL = url
-            match = re.search('ratemyprofessors.com', url)
-            if (match == None):
-                match = re.search('linkedin.com', url)
-                if (match == None):
-                    break
+            matched = 0
+            for t in trim:
+                match = re.search(t, url)
+                if (match != None):
+                    matched = matched + 1
+            if (matched == 0):
+                break
                         
         # Output the name and this resolved URL.
         match = re.search('www.google.com', actualURL)
+        name = name.encode('utf8')
         if (match == None):
             # Not a google link.
-            outfile.write(name.encode('utf8') + "," + actualURL + "\n")
-            print(name.encode('utf8') + "," + actualURL)
+            print(name + "," + actualURL)
+            outfile.write(name + "," + actualURL + "\n")
         else:
             if (not (name in homepages)):
                 # It's a new name, what are you gonna do (even if it is a
                 # Google link, include it).
-                outfile.write(name.encode('utf8') + "," + actualURL + "\n")
-                print(name.encode('utf8') + "," + actualURL)
+                print(name + "," + actualURL)
+                outfile.write(name + "," + actualURL + "\n")
             else:
                 print("Lookup failed for "+name+" -- found "+actualURL)
         sys.stdout.flush()
