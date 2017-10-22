b6d3fd0f130f9ee328cee0cfb85f44618b631d65
emeryberger <emery.berger@gmail.com>
2017-10-22 06:26:03 -0400

    Updated.

--- util/make-scholar-links.py
+++ util/make-scholar-links.py
@@ -1,7 +1,9 @@
 # Identify faculty home pages.
 #import google
 
+import os
 import scholarly
+from collections import *
 
 import codecs
 import sys
@@ -20,62 +22,123 @@ def csv2dict_str_str(fname):
         d = { rows[0].strip(): rows[1].strip() for rows in reader}
     return d
 
-import requests
 
 facultydict = csv2dict_str_str('faculty-affiliations.csv')
-homepages = csv2dict_str_str('scholar.csv')
+homepages1 = csv2dict_str_str('scholar.csv')
+aliases = csv2dict_str_str('dblp-aliases.csv')
+# Sort
+homepages = OrderedDict(sorted(homepages1.items(), key=lambda t: t[0]))
+
+visited = {}
+
+def getScholarID(name):
+    print "checking "+name
+    if name in homepages:
+        # Already there.
+        print "already there."
+        return homepages[name]
+    if name in visited:
+        return visited[name]
+    visited[name] = None
+    origname = name
+    # Trim off any trailing numerical suffixes.
+    r = re.match(".*\s\d\d\d\d$", name)
+    if r != None:
+        name = name[:-5]
+    actualID = "FIXME"
+    try:
+        search_query = scholarly.search_author(name)
+        name = name.decode('utf8')
+        author = next(search_query).fill()
+        # print author
+        for (key, value) in author.__dict__.items():
+            if (key == "id"):
+                actualID = value
+                break
+    except:
+        return None
+    if actualID == "FIXME":
+        return None
+    else:
+        visited[origname] = actualID
+        return actualID
+
+import requests
+
+# Working through top 20 US first, then the rest.
+schoolList = ["Carnegie Mellon University",
+              "Massachusetts Institute of Technology",
+              "Stanford University",
+              "University of California - Berkeley",
+              "University of Illinois at Urbana-Champaign",
+              "Cornell University",
+              "University of Michigan",
+              "University of Washington",
+              "Georgia Institute of Technology",
+              "University of California - San Diego",
+              "Columbia University",
+              "University of Wisconsin - Madison",
+              "University of Southern California",
+              "University of Pennsylvania",
+              "University of Texas at Austin",
+              "Princeton University",
+              "Purdue University",
+              "University of California - Los Angeles"]
+
+schools = {}
+for s in schoolList:
+    schools[s] = True
+
+with codecs.open("scholar2.csv", "w", "utf8") as outfile:
+    outfile.write("name,scholarid\n")
+    for name in homepages:
+        if (name == "name"):
+            continue
+        outfile.write(name.decode('utf8') + "," + homepages[name] + "\n")
+    outfile.flush()
+
+os.rename("scholar2.csv","scholar.csv")
 
 with codecs.open("scholar.csv", "a", "utf8") as outfile:
     facultydictkeys = list(facultydict.keys())
     random.shuffle(facultydictkeys)
     for name in facultydictkeys:
-        # Skip any homepages we have already in the database.
-        if (name in homepages):
-            # ...unless it's a Google search page, then we will try again to fix it.
-            # match = re.search('www.google.com', homepages[name])
-            # if (match == None):
+        if facultydict[name] not in schools:
             continue
-        # str = '"' + facultydict[name] + '" "' + name + '"'
+        # Skip any homepages we have already in the database.
+        if name in aliases:
+            name = aliases[name]
+            id = getScholarID(name)
+        if name in homepages:
+            continue
         dept = facultydict[name]
+        id = getScholarID(name)
+        if id == None:
+            # Try to resolve the alias.
+            if name in aliases:
+                name = aliases[name]
+                id = getScholarID(name)
+        if id == None:
+            # Try to remove a middle name.
+            r = re.match(".* [A-Z]\. *", name)
+            if r != None:
+                nomiddlename = re.sub(" [A-Z]\. ", " ", name)
+                id = getScholarID(nomiddlename)
+        if id == None:
+            print "NONE FOUND"
+            continue
         str = name + ", " + dept
         print str
-        # "University of Massachusetts Amherst" author:Emery author:Berger
-        # Grab first result from Google search.
-        #search_query = scholarly.search_author('Emery D. Berger, University of Massachusetts Amherst')
-        #print(next(search_query))
-        try:
-            actualURL = "FIXME"
-            search_query = scholarly.search_author(name)
+        if (not (name in homepages)):
+            # It's a new name, what are you gonna do (even if it is a
+            # Google link, include it).
             name = name.decode('utf8')
-            try:
-                author = next(search_query).fill()
-                # print author
-                id = ""
-                for (key, value) in author.__dict__.items():
-                    if (key == "id"):
-                        id = value 
-                        # https://scholar.google.com/citations?user=RaHaArkAAAAJ&hl=en&oi=ao
-                actualURL = id # "https://scholar.google.com/citations?user="+id+"&hl=en&oi=ao"
-                # print actualURL
-            except:
-                continue
+            outfile.write(name + "," + id + "\n")
+            outfile.flush()
+            print(name + "," + id)
+            actualURL = "https://scholar.google.com/citations?user="+id+"&hl=en&oi=ao"
+        else:
+            print("Lookup failed for "+name+" -- found "+id)
         
-            if actualURL == "FIXME":
-                continue
-        
-            if (not (name in homepages)):
-                # It's a new name, what are you gonna do (even if it is a
-                # Google link, include it).
-                outfile.write(name + "," + actualURL + "\n")
-                outfile.flush()
-                print(name + "," + actualURL)
-                actualURL = "https://scholar.google.com/citations?user="+actualURL+"&hl=en&oi=ao"
-            else:
-                print("Lookup failed for "+name+" -- found "+actualURL)
-        
-            sys.stdout.flush()
-            # Throttle lookups to avoid getting cut off by Google.
-            time.sleep(2.0)
-        except:
-            continue
+        sys.stdout.flush()
 
