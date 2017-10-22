3333651b0786e5518d0b338bf537f5f9ea825593
emeryberger <emery.berger@gmail.com>
2017-10-22 10:01:38 -0400

    More script refinement.

--- util/make-scholar-links.py
+++ util/make-scholar-links.py
@@ -24,22 +24,26 @@ def csv2dict_str_str(fname):
 
 
 facultydict = csv2dict_str_str('faculty-affiliations.csv')
-homepages1 = csv2dict_str_str('scholar.csv')
+scholarLinks1 = csv2dict_str_str('scholar.csv')
 aliases = csv2dict_str_str('dblp-aliases.csv')
 # Sort
-homepages = OrderedDict(sorted(homepages1.items(), key=lambda t: t[0]))
+scholarLinks = OrderedDict(sorted(scholarLinks1.items(), key=lambda t: t[0]))
 
-visited = {}
+checked = csv2dict_str_str('scholar-visited.csv')
+now = time.time()
+expirationDate = 60 * 60 * 7 # One week
 
 def getScholarID(name):
-    print "checking "+name
-    if name in homepages:
+    print "Checking "+name
+    if name in scholarLinks:
         # Already there.
-        print "already there."
-        return homepages[name]
-    if name in visited:
-        return visited[name]
-    visited[name] = None
+        print "Found"
+        return scholarLinks[name]
+    if name in checked:
+        if now - float(checked[name]) < expirationDate:
+            return None
+    else:
+        checked[name] = now
     origname = name
     # Trim off any trailing numerical suffixes.
     r = re.match(".*\s\d\d\d\d$", name)
@@ -54,14 +58,11 @@ def getScholarID(name):
         for (key, value) in author.__dict__.items():
             if (key == "id"):
                 actualID = value
-                break
+                scholarLinks[origname] = actualID
+                return actualID
     except:
         return None
-    if actualID == "FIXME":
-        return None
-    else:
-        visited[origname] = actualID
-        return actualID
+    
 
 import requests
 
@@ -91,54 +92,56 @@ for s in schoolList:
 
 with codecs.open("scholar2.csv", "w", "utf8") as outfile:
     outfile.write("name,scholarid\n")
-    for name in homepages:
+    for name in scholarLinks:
         if (name == "name"):
             continue
-        outfile.write(name.decode('utf8') + "," + homepages[name] + "\n")
+        outfile.write(name.decode('utf8') + "," + scholarLinks[name] + "\n")
     outfile.flush()
 
 os.rename("scholar2.csv","scholar.csv")
 
 with codecs.open("scholar.csv", "a", "utf8") as outfile:
-    facultydictkeys = list(facultydict.keys())
-    random.shuffle(facultydictkeys)
-    for name in facultydictkeys:
-        if facultydict[name] not in schools:
-            continue
-        # Skip any homepages we have already in the database.
-        if name in aliases:
-            name = aliases[name]
-            id = getScholarID(name)
-        if name in homepages:
-            continue
-        dept = facultydict[name]
-        id = getScholarID(name)
-        if id == None:
-            # Try to resolve the alias.
+    with codecs.open("scholar-visited.csv", "a", "utf8") as visitedFile:
+        facultydictkeys = list(facultydict.keys())
+        random.shuffle(facultydictkeys)
+        for name in facultydictkeys:
+            now = time.time()
+            if facultydict[name] not in schools:
+                continue
+            # Skip any scholarLinks we have already in the database.
             if name in aliases:
                 name = aliases[name]
-                id = getScholarID(name)
-        if id == None:
-            # Try to remove a middle name.
-            r = re.match(".* [A-Z]\. *", name)
-            if r != None:
-                nomiddlename = re.sub(" [A-Z]\. ", " ", name)
-                id = getScholarID(nomiddlename)
-        if id == None:
-            print "NONE FOUND"
-            continue
-        str = name + ", " + dept
-        print str
-        if (not (name in homepages)):
-            # It's a new name, what are you gonna do (even if it is a
-            # Google link, include it).
-            name = name.decode('utf8')
-            outfile.write(name + "," + id + "\n")
-            outfile.flush()
-            print(name + "," + id)
-            actualURL = "https://scholar.google.com/citations?user="+id+"&hl=en&oi=ao"
-        else:
-            print("Lookup failed for "+name+" -- found "+id)
+            if name in scholarLinks:
+                continue
+            if name in checked:
+                if now - float(checked[name]) < expirationDate:
+                    continue
+            s = "%10.2f" % time.time() 
+            visitedFile.write(name + "," + s + "\n")
+            visitedFile.flush()
+            dept = facultydict[name]
+            print "checking "+name+" at "+dept
+            id = getScholarID(name)
+            if id == None:
+                # Try to remove a middle name.
+                r = re.match(".* [A-Z]\. *", name)
+                if r != None:
+                    nomiddlename = re.sub(" [A-Z]\. ", " ", name)
+                    id = getScholarID(nomiddlename)
+            if id == None:
+                continue
+            str = name + ", " + dept
+            print str
+            if (not (name in scholarLinks)):
+                # It's a new name, what are you gonna do (even if it is a
+                # Google link, include it).
+                name = name.decode('utf8')
+                outfile.write(name + "," + id + "\n")
+                outfile.flush()
+                print(name + "," + id)
+                actualURL = "https://scholar.google.com/citations?user="+id+"&hl=en&oi=ao"
+            else:
+                print("Already there: "+name+" "+id)
         
-        sys.stdout.flush()
+            sys.stdout.flush()
 
