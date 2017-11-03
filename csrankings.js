d8c32c6d1fb315a77bd5a70f428fa452f5170645
emeryberger <emery.berger@gmail.com>
2017-11-03 10:24:05 -0400

    More abbreviations.

--- csrankings.js
+++ csrankings.js
@@ -598,7 +598,7 @@ var CSRankings = (function () {
             if (!deptNames.hasOwnProperty(dept)) {
                 return "continue";
             }
-            var p = '<div class="table"><table class="table-responsive table-sm table-striped"><thead><th></th><td><small><em><abbr title="Click on an author\'s name to go to their home page.">Faculty</abbr></em></small></td><td align="right"><small><em>&nbsp;&nbsp;<abbr title="Total number of publications (click for DBLP entry).">Raw&nbsp;\#&nbsp;Pubs</abbr></em></small></td><td align="right"><small><em><abbr title="Count divided by number of co-authors">Adjusted&nbsp;\#</abbr></em></small></td></thead><tbody>';
+            var p = '<div class="table"><table class="table-responsive table-sm table-striped"><thead><th></th><td><small><em><abbr title="Click on an author\'s name to go to their home page.">Faculty</abbr></em></small></td><td align="right"><small><em>&nbsp;&nbsp;<abbr title="Total number of publications (click for DBLP entry).">\#&nbsp;Pubs</abbr></em></small></td><td align="right"><small><em><abbr title="Count divided by number of co-authors">Adj.&nbsp;\#</abbr></em></small></td></thead><tbody>';
             /* Build a dict of just faculty from this department for sorting purposes. */
             var fc = {};
             for (var _i = 0, _a = deptNames[dept]; _i < _a.length; _i++) {
