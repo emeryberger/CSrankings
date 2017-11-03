dda99759275268296b24dc7cd593b8a8c42e0e63
emeryberger <emery.berger@gmail.com>
2017-11-03 15:06:59 -0400

    More formatting.

--- csrankings.js
+++ csrankings.js
@@ -116,7 +116,7 @@ var CSRankings = (function () {
     };
     /* Create the prologue that we preface each generated HTML page with (the results). */
     CSRankings.makePrologue = function () {
-        var s = '<div class="table-responsive" style="overflow:auto; height: 650px;">'
+        var s = '<div class="table-responsive" style="overflow:auto; height: 720px;">'
             + '<table class="table table-sm table-striped"'
             + 'id="ranking" valign="top">';
         return s;
@@ -598,7 +598,7 @@ var CSRankings = (function () {
             if (!deptNames.hasOwnProperty(dept)) {
                 return "continue";
             }
-            var p = '<div class="table"><table class="table-responsive table-sm table-striped"><thead><th></th><td><small><em><abbr title="Click on an author\'s name to go to their home page.">Faculty</abbr></em></small></td><td align="right"><small><em>&nbsp;&nbsp;<abbr title="Total number of publications (click for DBLP entry).">\#&nbsp;Pubs</abbr></em></small></td><td align="right"><small><em><abbr title="Count divided by number of co-authors">Adj.&nbsp;\#</abbr></em></small></td></thead><tbody>';
+            var p = '<div class="table"><table class="table table-sm table-striped"><thead><th></th><td><small><em><abbr title="Click on an author\'s name to go to their home page.">Faculty</abbr></em></small></td><td align="right"><small><em>&nbsp;&nbsp;<abbr title="Total number of publications (click for DBLP entry).">\#&nbsp;Pubs</abbr></em></small></td><td align="right"><small><em><abbr title="Count divided by number of co-authors">Adj.&nbsp;\#</abbr></em></small></td></thead><tbody>';
             /* Build a dict of just faculty from this department for sorting purposes. */
             var fc = {};
             for (var _i = 0, _a = deptNames[dept]; _i < _a.length; _i++) {
