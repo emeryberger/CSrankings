b53d34b8f7957f7def6da1d922d652ea23cbe314
emeryberger <emery.berger@gmail.com>
2018-05-05 08:37:10 -0400

    (Temporarily?) patched to work around DBLP issue: no page start/end for CHI 2018.

--- util/regenerate-data.py
+++ util/regenerate-data.py
@@ -489,6 +489,7 @@ def countPaper(confname, year, volume, number, startPage, pageCount, url, title)
         exceptionConference |= confname == 'SIGSOFT FSE' and year == 2012
         exceptionConference |= confname == 'ACM Trans. Graph.' and int(volume) >= 26 and int(volume) <= 36
         exceptionConference |= confname == 'SIGGRAPH' and int(volume) >= 26 and int(volume) <= 36
+        exceptionConference |= confname == 'CHI' and year == 2018 # FIXME - hopefully DBLP will fix
         if exceptionConference:
             tooFewPages = False
     if tooFewPages:
