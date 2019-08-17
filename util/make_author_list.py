7c0cf7cbdd066f353e26a942bb3c2cdb0e2aac85
emeryberger <emery.berger@gmail.com>
2019-08-17 14:47:21 -0700

    Sorted to make header and order canonical.

--- util/make_author_list.py
+++ util/make_author_list.py
@@ -2,49 +2,44 @@
 # the CSV is of the following format:
 #   name, confnameYear (first year published in that conference), confnameCount (total pubs in that conference), ... (with a final dummy field for convenience)
 
-import gzip
-import xmltodict
-import collections
 import json
 import csv
-import re
-import sys
-import operator
-# from typing import Dict
-from csrankings import *
 
 filename = 'articles.json';
 
 with open(filename, 'r') as f:
     datastore = json.load(f)
 
+# Change this to use other conferences. Note that if they aren't indexed by CSrankings, they won't appear.
 confdict = { "PLDI", "POPL", "ICFP", "OOPSLA" }
 conflist = list(confdict)
+conflist.sort()
 
-lastconf = ""
-
+# Generate the header.
 print("name,", end='')
 for k in conflist:
     print(k+","+k+"year"+","+k+"count"+",",end='')
 print("dummy")
-    
-i = 0
-year = {}
-count = {}
 
+# Now start iterating through the data.
+
+i = 0      # which entry we are on in the data (stored as an array)
+year = {}  # dict: year[name][conf] = earliest year published by name in that conference
+count = {} # dict: count[name][conf] = total count of pubs by name in that conference
+
+# Note that articles.json is sorted by author name and then by date.
 while (i < len(datastore)):
-    # Note that articles.json is sorted by author name and then by date.
     name = datastore[i]["name"]
-    # print(name)
     year[name] = {}
     count[name] = {}
+    # Collect all conference data for this author.
     while ((i < len(datastore)) and (datastore[i]["name"] == name)):
         conf = datastore[i]["conf"]
         if (conf in confdict):
-            # print("----" + conf + ", " + str(datastore[i]["year"]))
             year[name][conf] = min(year[name].get(conf, 99999), datastore[i]["year"])
             count[name][conf] = count[name].get(conf, 0) + 1
         i += 1
+    # Spit out a CSV record.
     print(name + ",", end='')
     for c in conflist:
         if c in year[name]:
@@ -52,11 +47,4 @@ while (i < len(datastore)):
         else:
             print("0,0,", end='')
     print('NA')
-    #print(name + " " + year[name] + " " + count[name])
-#    print(datastore[i])
 
-def main():
-    pass
-
-if __name__== "__main__":
-  main()
