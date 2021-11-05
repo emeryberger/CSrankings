45c4bf63d6401eb08b842b4b6a6722a26d9ae910
Emery Berger <emery.berger@gmail.com>
2021-11-05 19:16:23 -0400

    Added dept toggle analytics.

--- csrankings.ts
+++ csrankings.ts
@@ -1457,7 +1457,7 @@ class CSRankings {
 
                 s += "&nbsp;" + `<span onclick="csr.toggleFaculty('${esc}');">${dept}</span>`
 		  + `&nbsp;<img src="/flags/${abbrv}.png">&nbsp;`
-                    + "<span class=\"hovertip\" onclick=\"csr.toggleChart('" + esc + "');\" id=\"" + esc + "-chartwidget\">"
+                    + `<span class="hovertip" onclick='csr.toggleChart("${esc}"); ga("send", "event", "chart", "toggle-department", "toggle ${esc} ${$("#charttype").find(":selected").val()} chart");' id='${esc + "-chartwidget"}'>`
                     + this.ChartIcon + "</span>";
                 s += "</td>";
 
