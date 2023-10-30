f3b868efa9a2521debaadf612fee078360621085
Daniel Khashabi <danielk@jhu.edu>
2023-10-30 10:48:29 -0400

    CS @ JHU  (#5945)
    * Anjalie and Eric.
    
    * Renjie.
    
    * minor correction.
    
    * Adding the remainder of the affiliates.
    
    * minor.
    
    * minor.
    
    * minor.
    
    * minor.
    
    * minor.
    
    * minor.
    
    * Ziang and Anjalie
    
    * Ziang and Anjalie
    
    * adding alex marder
    
    * minor.
    
    * minor.
    
    * add comments to the validation file.
    
    * add comments to the validation file.
    
    * add comments to the validation file.
    
    * add comments to the validation file.
    
    * drop the old comments.
    
    * minor.
    
    ---------
    
    Co-authored-by: Emery Berger <emery.berger@gmail.com>

--- validate_commit.py
+++ validate_commit.py
@@ -4,6 +4,7 @@ import json
 import re
 import requests
 import sys
+import time
 import urllib.parse
 allowed_files = ['csrankings-[a-z0].csv', 'country-info.csv', 'old/industry.csv', 'old/other.csv', 'old/emeritus.csv', 'old/rip.csv']
 
@@ -114,6 +115,10 @@ def matching_name_with_dblp(name: str) -> int:
         # Send a request to the DBLP API.
         response = requests.get(dblp_url)
         # Extract the number of completions from the JSON response.
+        if "<title>429 Too Many Requests</title>" in response.text:
+            # wait for a few seconds and try again
+            time.sleep(10)
+            return matching_name_with_dblp(name)
         j = json.loads(response.text)
         completions = int(j['result']['completions']['@total'])
         # Print a message if there is a match.
