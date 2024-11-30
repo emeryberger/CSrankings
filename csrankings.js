2e1ca7b1e454e5e387c756f777390d247f62ea02
Emery Berger <emery.berger@gmail.com>
2024-11-30 10:11:05 -0500

    Sort faculty with same number of pubs by descending adjusted count. Fixes https://github.com/emeryberger/CSrankings/issues/5030.

--- csrankings.js
+++ csrankings.js
@@ -1016,13 +1016,15 @@ class CSRankings {
             let keys = Object.keys(fc);
             keys.sort((a, b) => {
                 if (fc[b] === fc[a]) {
-                    return this.compareNames(a, b);
-                    /*		    let fb = Math.round(10.0 * facultyAdjustedCount[b]) / 10.0;
-                            const fa = Math.round(10.0 * facultyAdjustedCount[a]) / 10.0;
-                            if (fb === fa) {
-                            return this.compareNames(a, b);
-                            }
-                            return fb - fa; */
+                    // return this.compareNames(a, b);
+                    const fb = Math.round(10.0 * facultyAdjustedCount[b]) / 10.0;
+                    const fa = Math.round(10.0 * facultyAdjustedCount[a]) / 10.0;
+                    if (fb === fa) {
+                        return this.compareNames(a, b);
+                    }
+                    else {
+                        return fb - fa;
+                    }
                 }
                 else {
                     return fc[b] - fc[a];
