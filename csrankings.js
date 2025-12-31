a3b44f22d0ee8f828c0aec80162a3461c65b1cf7
Emery Berger <emery.berger@gmail.com>
2025-12-31 17:30:23 -0500

    Remove overly aggressive iframe error detection

--- csrankings.js
+++ csrankings.js
@@ -3949,37 +3949,11 @@ var CSRankings;
     function loadPreviewContent(url, container) {
         const iframe = document.createElement('iframe');
         iframe.sandbox.add('allow-scripts', 'allow-same-origin');
-        let errorShown = false;
-        const showError = () => {
-            if (!errorShown) {
-                errorShown = true;
-                showPreviewError(container, url);
-            }
-        };
-        iframe.onload = () => {
-            var _a;
-            // Check if iframe actually loaded content
-            try {
-                // This will throw if blocked by X-Frame-Options (cross-origin)
-                const doc = iframe.contentDocument || ((_a = iframe.contentWindow) === null || _a === void 0 ? void 0 : _a.document);
-                if (!doc || !doc.body || doc.body.innerHTML === '') {
-                    showError();
-                }
-            }
-            catch (_b) {
-                // Cross-origin error means the site likely blocked framing
-                showError();
-            }
-        };
         iframe.onerror = () => {
-            showError();
+            showPreviewError(container, url);
         };
-        // Timeout for slow loads
-        setTimeout(() => {
-            if (container.querySelector('.homepage-preview-loading')) {
-                showError();
-            }
-        }, 2000);
+        // Just load the iframe - if the site blocks framing, the browser
+        // will show its own error. We can't reliably detect this from JS.
         iframe.src = url;
         container.innerHTML = '';
         container.appendChild(iframe);
